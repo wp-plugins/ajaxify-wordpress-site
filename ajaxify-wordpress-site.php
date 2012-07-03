7454e617fd8b0b9ca0cdc6547c77b7c88c09e876
manishkrag <manishkrag@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-03 05:42:51 +0000

    Added the plugin filles to the SVN at 11 AM
    git-svn-id: https://plugins.svn.wordpress.org/ajaxify-wordpress-site/trunk@566778 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajaxify-wordpress-site.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ *Plugin Name: Ajaxify Wordpress Site
+ *Description: This will ajaxify your website. All the front end links will turns to ajaxify.
+ *Author: Manish Kumar Agarwal
+ *EmailId: manishkrag@yahoo.co.in/manisha@mindfiresolutions.com
+ *Version: 1.0
+ */
+ 
+/*
+ ---------------------------------------------------------------------
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+---------------------------------------------------------------------
+*/
+ 
+//Loading js files
+function load_scripts() {
+	
+	//Check whether the core jqury library enqued or not. If not enqued the enque this
+	if(!wp_script_is( 'jquery' )) {
+		wp_enqueue_script('jquery');
+	}
+	$plugin_dir_path = plugin_dir_url(__FILE__);
+	wp_enqueue_script('history-js', $plugin_dir_path . 'js/history.js', array('jquery'));
+	wp_enqueue_script('jquery-scrollTo-js', $plugin_dir_path . 'js/jquery.scrollTo-min.js', array('jquery'));
+	wp_enqueue_script('ajaxify-js',  $plugin_dir_path . 'js/ajaxify.js', array('jquery'));
+}
+//calling load_scripts function to load js files
+add_action('wp_enqueue_scripts', 'load_scripts');
---
+++ readme
@@ -0,0 +1,22 @@
+=== Ajaxify Wordpress Site ===
+Contributors: manishkrag
+Version 1.0.0
+Tags: ajax, posts, pages, page, post, ajax search, ajaxify
+Tested up to: 3.4
+
+== Description ==
+
+Ajaxify Wordpress Site will load posts, pages, search etc. without reloading entire page. This was my first plugin and is still a little quirky.
+
+== Installation ==
+
+1. Upload `ajaxify-wordpress-site` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress. 
+
+== Frequently Asked Questions ==
+Q: My site not ajaxify even after activating this plugin?
+A: Please check you core jQuery library, please use 1.7 version.
+Q: Search is not working as ajaxify?
+A: Please make sure that your search form submit input tag id should be 'search-submit' and input box id should be 'search-terms'.
+Q: I don't want to make a link ajaxify, what to do?
+A: Add 'no-ajaxify' class to the 'a' tag which you dont want to make ajaxify.
